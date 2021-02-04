a1d0852534ce959378a5a7606a844cf0e9a0fd6e
mikohatsu <allenhong36@gmail.com>
2021-02-04 23:40:16 +0900

    합쳤다

--- yachu.py
+++ yachu.py
@@ -5,6 +5,7 @@ import sys
 from PyQt5.QtGui import *
 import random
 from yachupkg.MyLabel import *
+#from yachupkg.server import server
 
 yachu = '.\\Yachu.ui'
 
@@ -13,25 +14,24 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(yachu, self)
+
         self.pushButton.clicked.connect(self.rolldice)               # 주사위굴리기 버튼
         self.tableWidget.cellClicked.connect(self.inputrank)         # 점수표 클릭 연결
+
         self.actionCreate_Game.triggered.connect(self.createServer)  # 상단메뉴바의 멀티게임생성 트리거
         self.actionJoin_Game.triggered.connect(self.createServer)    # 상단메뉴바의 멀티게임참가 트리거
+
         self.dice = [self.label1, self.label2, self.label3, self.label4, self.label5]  # 게임주사위들을 저장한 배열
         self.ran_num = [0, 0, 0, 0, 0]
         for i in self.dice:
             i.clicked.connect(self.keep)
+
         self.qp = QPixmap()
         for i in range(0, 5):
-                self.qp.load(f'dice{i+1}.png')
+                self.qp.load(f'./img/dice{i+1}.png')
                 self.dice[i].setPixmap(self.qp)
-        # self.loadingUI()
-        self.cancelbtn.clicked.connect(self.stopServer)              # 서버로딩창의 취소버튼
-        self.loadgif = QMovie(f'loading.gif')
-        self.label_loading.setMovie(self.loadgif)
-        self.loadgif.start()
-        self.frame.setStyleSheet("background-color:white;")
-        self.frame.hide()
+
+        self.loadingUI()
         self.show()
 
     def loadingUI(self):
@@ -39,6 +39,7 @@ class MyWindow(QMainWindow):
         self.loadgif = QMovie(f'./img/loading.gif')
         self.label_loading.setMovie(self.loadgif)
         self.loadgif.start()
+        self.frame.setStyleSheet("background-color:white;")
         self.frame.hide()
 
     def rolldice(self):
@@ -46,7 +47,7 @@ class MyWindow(QMainWindow):
         for i in range(0, 5):
             if self.dice[i].frameShape() == QFrame.NoFrame:
                 self.ran_num[i] = random.randrange(1, 7)
-                self.qp.load(f'dice{self.ran_num[i]}.png')
+                self.qp.load(f'./img/dice{self.ran_num[i]}.png')
                 self.dice[i].setPixmap(self.qp)
         self.handrank()
 
